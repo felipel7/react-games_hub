08b188e897d5375010007e5a5203fb3fa501ed15
felipel7 <felipelseven@gmail.com>
2023-09-06 10:40:54 -0300

    chore(deps): add axios

---
+++ src/services/api-client.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  params: {
+    key: import.meta.env.VITE_API_KEY,
+  },
+});
+
+export { apiClient };

b326a80fd2ab9e9a188afb1d357be9a39d8dc1cf
felipel7 <felipelseven@gmail.com>
2023-09-06 04:16:09 -0300

    style: create layout initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+
 function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">Nav</GridItem>
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
+      </Show>
+      <GridItem area="main">Main</GridItem>
+    </Grid>
+  );
 }
 
 export default App;

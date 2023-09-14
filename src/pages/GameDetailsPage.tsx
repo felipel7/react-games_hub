ddfeb148d2a746808e8595365ec8592dd60962c2
felipel7 <felipelseven@gmail.com>
2023-09-14 10:21:18 -0300

    feat(ExpandableText): create component structure

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,5 +1,6 @@
-import { Heading, Spinner, Text } from '@chakra-ui/react';
+import { Heading, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
+import ExpandableText from '../components/ExpandableText';
 import useGame from '../hooks/useGame';
 
 function GameDetailsPage() {
@@ -13,7 +14,7 @@ function GameDetailsPage() {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 }
